99a43081e8968316686a92f5f5bbb17dd96c9021
CyberWendy123 <xiaowenx@alumni.wfu.edu>
2019-09-10 10:06:12 -0400

    first

---
+++ myStack.h
@@ -0,0 +1,14 @@
+/*
+* stack.h 
+*
+* created on: Sept 10, 2019 
+*	Author: Wendy Xiao 
+*/ 
+#ifndef MYSTACK_H_ 
+#define MYSTACK_H_
+
+class myStack{
+
+}; 
+
+#endif 
---
+++ stackMain.cpp
@@ -0,0 +1,16 @@
+/*
+* stackMain.cpp 
+*
+* created on: Sept 10, 2019 
+*	Author: Wendy Xiao 
+*/ 
+
+// implementing a stack with push and pop functions (9/8/19)
+#include <iostream> 
+#include "stack.h"
+using namespace std; 
+
+int main()
+{
+	return 0; 
+}
